c6e6be0b7c96eeb40e4b97c76adefd78c2036e84
customaddon <customaddon@gmail.com>
2016-10-26 16:03:01 -0400

    Shopify Connection

---
+++ config/initializers/shopify_app.rb
@@ -0,0 +1,6 @@
+ShopifyApp.configure do |config|
+  config.api_key = "2cc69b71b36f8b38ad966d84301b90c3"
+  config.secret = "9bf641403020b555f98f8d04e96ddd42"
+  config.scope = "read_orders, read_products"
+  config.embedded_app = true
+end
